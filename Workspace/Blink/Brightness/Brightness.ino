9df628efff52f9fbec6bd4fb00b6de9758f25896
Yohann <yohann.colman@hei.fr>
2017-04-21 15:20:12 +0200

    Brightness

--- Workspace/Blink/Brightness/Brightness.ino
+++ Workspace/Blink/Brightness/Brightness.ino
@@ -20,17 +20,26 @@
   by Colby Newman
 */
 
-
+int brightness=0;
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  while(brightness<=260){
+    if (brightness==260)
+  {
+    brightness=0;
+  }
+  else {
+    Serial.println(brightness);
+    analogWrite(5, brightness);  // turn the LED on (HIGH is the voltage level)
+    delay(500);// wait
+    brightness+=5;
+    }
+  }
 }
